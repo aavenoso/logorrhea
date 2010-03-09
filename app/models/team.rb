eda65aca294c8eb9538a168c86f4168b4bdf7fc2
Brent Sowers <brentsowers1@gmail.com>
2010-03-09 16:26:51 -0500

    added message limit

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,10 @@ class Team < ActiveRecord::Base
   validates_presence_of :name
   
   has_many :users
-  has_many :messages, :through => :users
+  has_many :messages, :through => :users do
+    def most_recent(count=5)
+      all(:order => "messages.created_at desc", :limit => count)
+    end
+  end
 
 end
