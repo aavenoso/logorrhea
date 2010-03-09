733f84931f2348c678512c40b845606922b77e51
Andrew Avenoso <aavenoso@nearinfinity.com>
2010-03-09 16:42:17 -0500

    Gravatar Support

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
+  map.resources :users
+
   map.resources :pipes
 
   # The priority is based upon order of creation: first created -> highest priority.
