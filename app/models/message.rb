762eb09b60fa2f8503acfda7f2ba1ab244b749ed
robstclair <rstclair@intrexon.com>
2010-03-09 16:24:31 -0500

    Adding named scopes :urgent and :recent. Chain to get recent.urgent as per spec.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,20 @@
 class Message < ActiveRecord::Base
+
+  PRIORITIES = [:urgent, :important, :casual, :fun]
+
   belongs_to :user
   belongs_to :parent
   has_many :children, :class_name  => :message, :foreign_key => :parent_id, :dependent => :destroy
   
-  PRIORITIES = [:urgent, :important, :casual, :fun]
+  named_scope :urgent,
+              :conditions => "priority = #{PRIORITIES.index(:urgent)}",
+              :limit => :all,
+              :order => "created_at DESC"
+  named_scope :recent,
+              lambda { |limit = 15, *|
+                {:limit => limit, :order => "created_at DESC"}
+              }
+              
   
   validates_presence_of :body
   validates_presence_of :user_id
