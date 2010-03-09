c9321cc84d5cc415d7b399c4d5657bbbfbc0fcf3
Matt Freivald <mpfreivald@gmail.com>
2010-03-09 16:09:14 -0500

    Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,19 @@
 class Message < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :parent
+  has_many :children, :class_name  => :message, :foreign_key => :parent_id, :dependent => :destroy
+  
+  PRIORITIES = [:urgent, :important, :casual, :fun]
+  
+  validates_presence_of :body
+  validates_presence_of :user_id
+  validates_length_of :body, :within => 2..140
+  
+  def priority
+    PRIORITIES[self[:priority]]
+  end
+  
+  def priority=(sym)
+    self[:priority] = PRIORITIES.index(sym)
+  end
 end
