67dae6d88cf2b3589da19f6fe6886531f05a1c62
Brent Sowers <brentsowers1@gmail.com>
2010-03-10 13:08:17 -0500

    Added accepts nested params to team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,11 +1,16 @@
 class Team < ActiveRecord::Base
   validates_presence_of :name
-  
+
   has_many :users
+
+  accepts_nested_attributes_for :users, :allow_destroy => true
+
   has_many :messages, :through => :users do
     def most_recent(count=5)
       all(:order => "messages.created_at desc", :limit => count)
     end
   end
+
+
 
 end
