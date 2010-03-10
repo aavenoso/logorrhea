b86c7c4231a70b7569ff0b689ea618ed4d29d369
Brent Sowers <brentsowers1@gmail.com>
2010-03-10 13:13:40 -0500

    Added delete to user form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def delete_child_option(form)
+    unless form.object.new_record?
+      form.check_boc("_delete") + form.label("_delete", "Delete")
+    end
+  end
 end
